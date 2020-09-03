8587e950723705fc0fa1ac491c18553559da22d0
meullah <mehsanullah24@gmail.com>
2020-09-03 21:57:07 +0500

    Updates

---
+++ precache-manifest.561e3d5890b59bc5eccfd22898a2f59e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe2f36a7ba922f24309fc93c22c406b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ed8a0b4a7bb81a7280",
+    "url": "/static/css/main.669ac10a.chunk.css"
+  },
+  {
+    "revision": "7729da24cb14344f8f6e",
+    "url": "/static/js/2.726bd328.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.726bd328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5ed8a0b4a7bb81a7280",
+    "url": "/static/js/main.a29d9c90.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "fb87c558b88a95a962173a773f68f287",
+    "url": "/static/media/pp.fb87c558.jpg"
+  },
+  {
+    "revision": "7be4bfeebbfe248170661541d3446e74",
+    "url": "/static/media/resume.7be4bfee.pdf"
+  }
+]);
